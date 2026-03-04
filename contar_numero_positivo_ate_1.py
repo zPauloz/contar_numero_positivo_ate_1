9325d6bdfda0af7f002fc697a0ba8a94010008dd
paulo <pereirapaulo59p@gmail.com>
2026-03-04 18:19:31 -0300

    contar_numero_positivo_ate_1

---
+++ contar_numero_positivo_ate_1.py
@@ -0,0 +1,9 @@
+numero = int(input("digite um numero positivo"))
+while numero <= 0:
+    numero = int(input("digite um numero positivo"))
+
+
+contador = numero
+while contador >= 1:
+    print(contador)
+    contador = contador - 1
